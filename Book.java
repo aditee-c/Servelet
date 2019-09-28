9188db843001b288c2fdfa5af4afa0f76ff20363
Aditee Chourasiya <aditee.chourasiya30@gmail.com>
2019-09-28 15:05:41 +0530

    Added Book.java

--- Book.java
+++ Book.java
@@ -2,4 +2,17 @@ class Book{
 	private int bookId;
 	private String name;
 	
+	public void getBookId(int bookId){
+		this.bookId= bookId;
+	}
+	public setBookId(int bookId){
+		return bookId;
+	}
+	
+	public void getBookId(String name){
+		this.name= name;
+	}
+	public setBookId(String name){
+		return name;
+	}
 }
